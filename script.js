ff2e937253901c55a68327077cc95bbe5047f2aa
cpblack <caleb.paul.black@gmail.com>
2016-11-14 14:03:09 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-function readTextFile(file)
+function readTextFile(filein)
 {
-    document.getElementById(file).innerHTML = "Loading..."
+    document.getElementById(filein).innerHTML = "Loading..."
     var rawFile = new XMLHttpRequest();
-    rawFile.open("GET", "/file/"+file+".txt", false);
+    rawFile.open("GET", "/file/"+filein+".txt", false);
     rawFile.onreadystatechange = function (){
         if(rawFile.readyState === 4)
         {
             if(rawFile.status === 200 || rawFile.status == 0)
             {
                 var allText = rawFile.responseText;
-                document.getElementById(file).innerHTML = allText
+                document.getElementById(filein).innerHTML = allText
             } else {
-                document.getElementById(file).innerHTML = "Failed."
+                document.getElementById(filein).innerHTML = "Failed."
             }
         }
     }
