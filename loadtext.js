d31a62f3126f8130325d54c2013c29518c745416
cpblack <caleb.paul.black@gmail.com>
2016-11-14 13:08:30 -0700

    Update loadtext.js

--- loadtext.js
+++ loadtext.js
@@ -4,8 +4,8 @@ function getText(name){
   client.open('GET', '/text/'+name+'.txt');
   client.onreadystatechange = function() {
     if (client.readyState == 4) {
-      var response = JSON.parse(xmlhttp.responseText);
-      if (xmlhttp.status === 200 && response.status === 'OK') {
+      var response = JSON.parse(client.responseText);
+      if (client.status === 200 && response.status === 'OK') {
         document.getElementById(name).innerHTML = client.responseText;
       } else {
         document.getElementById(name).innerHTML = "Failed to load."
