9d24c0d755980195a7c610dae75d4bffd4b23d38
cpblack <caleb.paul.black@gmail.com>
2016-11-14 12:53:07 -0700

    Update loadtext.js

--- loadtext.js
+++ loadtext.js
@@ -2,8 +2,15 @@ function getText(){
   var client = new XMLHttpRequest();
   client.open('GET', '/text/ilovevideogames.txt');
   client.onreadystatechange = function() {
-    if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+  if (xmlhttp.readyState === 4) {
+    var response = JSON.parse(xmlhttp.responseText);
+      if (xmlhttp.status === 200 && response.status === 'OK') {
     document.getElementById("ilovevideogames").innerHTML = client.responseText;
+      } else {
+    document.getElementById("ilovevideogames").innerHTML = "Failed to load."
+      }
+  }
+    if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
         }
   }
   client.send();
