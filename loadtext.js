182900d63236ddd787c7350966dcc76b40fbcd25
cpblack <caleb.paul.black@gmail.com>
2016-11-14 12:50:50 -0700

    Update loadtext.js

--- loadtext.js
+++ loadtext.js
@@ -2,7 +2,9 @@ function getText(){
   var client = new XMLHttpRequest();
   client.open('GET', '/text/ilovevideogames.txt');
   client.onreadystatechange = function() {
+    if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
     document.getElementById("ilovevideogames").innerHTML = client.responseText;
+        }
   }
   client.send();
 }
