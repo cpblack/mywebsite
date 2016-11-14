3008a3808dd1a293d7c8fac4122a6784b025e7b0
cpblack <caleb.paul.black@gmail.com>
2016-11-14 13:34:45 -0700

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ function getText(stringIn){
   var client = new XMLHttpRequest();
   client.open('GET', '/text/'+stringIn+'.txt');
   client.onreadystatechange = function() {
-    if (xhr.readyState == XMLHttpRequest.DONE || xhr.readyState == 4) {
-      var response = JSON.parse(xmlhttp.responseText);
-      if (xmlhttp.status === 200 && response.status === 'OK') {
+    if (client.readyState == XMLHttpRequest.DONE || client.readyState == 4) {
+      var response = JSON.parse(client.responseText);
+      if (client.status === 200 && client.status === 'OK') {
         document.getElementById(stringIn).innerHTML = client.responseText;
       } else {
         document.getElementById(stringIn).innerHTML = "Failed to load.";
