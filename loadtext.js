2bcaa270d21e0f9a0975c449a8022120efda6193
cpblack <caleb.paul.black@gmail.com>
2016-11-14 13:11:13 -0700

    Update loadtext.js

--- loadtext.js
+++ loadtext.js
@@ -1,19 +1,15 @@
-function getText(name){
-  document.getElementById(name).innerHTML = "Loading...";
+function getText(){
   var client = new XMLHttpRequest();
-  client.open('GET', '/text/'+name+'.txt');
+  client.open('GET', '/text/ilovevideogames.txt');
   client.onreadystatechange = function() {
-    if (client.readyState == 4) {
-      var response = JSON.parse(client.responseText);
-      if (client.status === 200 && response.status === 'OK') {
-        document.getElementById(name).innerHTML = client.responseText;
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      var response = JSON.parse(xmlhttp.responseText);
+      if (xmlhttp.status === 200 && response.status === 'OK') {
+        document.getElementById("ilovevideogames").innerHTML = client.responseText;
       } else {
-        document.getElementById(name).innerHTML = "Failed to load."
+        document.getElementById("ilovevideogames").innerHTML = "Failed to load."
       }
     }
   }
   client.send();
-}
-function loadTexts(){
-  getText("ilovevideogames");
 }
