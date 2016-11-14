3c81aab56e3d55184174c883c613b96e8cbe7374
cpblack <caleb.paul.black@gmail.com>
2016-11-14 13:32:19 -0700

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function getText(stringIn){
   var client = new XMLHttpRequest();
   client.open('GET', '/text/'+stringIn+'.txt');
   client.onreadystatechange = function() {
-    if (xhr.readyState === XMLHttpRequest.DONE) {
+    if (xhr.readyState == XMLHttpRequest.DONE || xhr.readyState == 4) {
       var response = JSON.parse(xmlhttp.responseText);
       if (xmlhttp.status === 200 && response.status === 'OK') {
         document.getElementById(stringIn).innerHTML = client.responseText;
