2fcace13ed4ef89f6eddb4b2c1dc1ee3f3b4c112
cpblack <caleb.paul.black@gmail.com>
2016-11-15 11:32:06 -0700

    Update script.js

--- script.js
+++ script.js
@@ -4,9 +4,7 @@ txtFile.open("GET", "http://polished.tech/text/"+stringName+".txt", true);
 txtFile.onreadystatechange = function() {
   if (txtFile.readyState === 4) {  // Makes sure the document is ready to parse.
     if (txtFile.status === 200) {  // Makes sure it's found the file.
-      allText = txtFile.responseText; 
-      lines = txtFile.responseText.split("\n");
-      onLoad(stringName,lines.join("<br>");
+      onLoad(stringName, JSON.parse(txtFile.responseText););
     }
   }
 }
