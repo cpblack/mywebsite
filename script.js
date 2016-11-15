8e6edad5c6d0d53a5e9209590742d90b9b38a8ad
cpblack <caleb.paul.black@gmail.com>
2016-11-15 11:00:54 -0700

    Update script.js

--- script.js
+++ script.js
@@ -4,19 +4,17 @@ function readTextFile(filein)
     var rawFile = new XMLHttpRequest();
     rawFile.open("GET", "/file/"+filein+".txt", false);
     rawFile.onreadystatechange = function (){
-        if(rawFile.readyState === 4)
-        {
-            if(rawFile.status === 200 || rawFile.status == 0)
-            {
+        if(rawFile.readyState === 4) {
+            if(rawFile.status === 200 || rawFile.status == 0) {
                 var allText = rawFile.responseText;
                 document.getElementById(filein).innerHTML = allText
             } else {
                 document.getElementById(filein).innerHTML = "Failed."
             }
         }
-    }
     rawFile.send(null);
 }
+
 function index(){
   readTextFile("ilovevideogames");
 }
