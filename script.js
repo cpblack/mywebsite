bbfa6211b8cf5058bb3f96153facc691566798d7
cpblack <caleb.paul.black@gmail.com>
2016-11-16 18:57:26 -0700

    Update script.js

--- script.js
+++ script.js
@@ -5,13 +5,18 @@ txtFile.onreadystatechange = function() {
   if (txtFile.readyState === 4) {  // Makes sure the document is ready to parse.
     if (txtFile.status === 200) {  // Makes sure it's found the file.
       onLoad(stringName, JSON.parse(txtFile.responseText));
+    } else {
+      onFail(stringName);
     }
   }
 }
 txtFile.send(null);
 }
 function onLoad(stringIn, content){
-document.getElementById(stringIn).innerHTML = content
+document.getElementById(stringIn).innerHTML = content;
+}
+function onFail(stringIn){
+console.log(stringIn+": Failure.");
 }
 
 function index(){
