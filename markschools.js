b67c87efce21229f28839ad8d143d516b2175f0d
cpblack <caleb.paul.black@gmail.com>
2017-04-29 20:00:13 -0600

    Add files via upload

--- markschools.js
+++ markschools.js
@@ -1,15 +1,15 @@
 var maxYear = 150;
-function getBaseSchools() {
-var baseSchools = [{name:"AISU"}]
-return JSON.parse(JSON.stringify(baseSchools));
+function duplicate(object) {
+return JSON.parse(JSON.stringify(object));
 }
 
 function buttonSubmit(){
 var value = document.getElementById("yearInput").value;
 value = Math.min(Math.max(document.getElementById("yearInput").value,1),maxYear);
 document.getElementById("yearInput").value = value;
-var calc = example(value);
+var calc = run(value - 1);
 console.log(calc);
+//console.log(getRevenueCostSet(calc));
 //setLeftovers(calc[1]);
 }
 function increase(){ 
@@ -24,27 +24,87 @@ function decrease(){
       buttonSubmit();
     }
 }
-function example(years){
-  console.log(years);
+function run(years){
+  //console.log(years);
   var get = getSchools(years);
-  return get;
+  var revenue = getRevenueCostSet(get);
+  addAISURevenue(revenue);
+  generateStatistics(years +1,revenue,getSchoolCount(get));
+  return revenue;
+}
+function addAISURevenue(revenue){
+    revenue[0] += 200000;
+    return revenue;
 }
 
-function getRevenueCost(schoolCount,age){
-    
-    
+function getSchoolCount(schoolSet){
+    var total = 1;
+    for (var i = 0; i < schoolSet.length; i++){
+        total += schoolSet[i].quantity;
+    }
+    return total;
 }
-
+function getRevenueCost(schools){
+    var students = 600;
+    var softwareRevenuePerStudent = 125;
+    var softwareLicenseRevenue = students * softwareRevenuePerStudent;
+    var retention = [100,90,75,50,25];
+    var furtherRetention = 25;
+    var consultingMargin = 40;
+    var softwareMargin = 95;
+    var baseOperations = 850000;
+    var firstYearConsultingRevenue = 150000;
+    var consecutiveYearConsultingRevenue = 75000;
+    
+    
+    var consultingRevenue = 0;
+    var softwareRevenue = 0;
+    var consultingCost = 0;
+    var softwareCost = 0;
+    //console.log(schools);
+    if (true || schools != null && schools.quantity != null && Number.isInteger(schools.quantity)&& schools.age != null && Number.isInteger(schools.age)){
+        //console.log(schools.year);
+        if (schools.year < 1){
+            consultingRevenue = firstYearConsultingRevenue * schools.quantity;
+        } else if (schools.year > 0){
+            consultingRevenue = consecutiveYearConsultingRevenue * schools.quantity;
+        }
+        consultingCost = consultingRevenue * ((100 - consultingMargin)/100);
+        var tempRetention = 0;
+        if (schools.year < retention.length) {
+            tempRetention = retention[schools.year];
+        } else {
+            tempRetention = furtherRetention;
+        }
+        softwareRevenue = softwareLicenseRevenue * (tempRetention / 100);
+        softwareCost = softwareRevenue * ((100 - softwareMargin)/100);
+        }
+    return [consultingRevenue,softwareRevenue,consultingCost,softwareCost];
+}
+function getRevenueCostSet(schoolsSet){
+    var output = [0,0,0,0];
+    for (var i = 0; i < schoolsSet.length;i++){
+        var fetch = getRevenueCost(schoolsSet[i]);
+        console.log(i+":");
+        console.log(fetch);
+        output[0] += fetch[0];
+        output[1] += fetch[1];
+        output[2] += fetch[2];
+        output[3] += fetch[3];
+    }
+    return output;
+}
 function getSchools(year) {
   var currentYear = 0;
   var currentFunds = 0;
-  var schools = [{year:0,quantity:1}]
-  while (currentYear < year) {
+  var schools = [];
+  while (currentYear < year + 1) {
      if (currentYear > 0){
-        var temp = {year:year,quantity:(3*year)};
+         var quantity = 3 * currentYear;
+        var temp = {year:year,quantity:quantity};
+        age(schools,1);
         schools.push(temp);
      }
-     age(schools,1);
      currentYear += 1;
   }
   
@@ -59,9 +119,10 @@ function age(schools,years){
 
 
 window.addEventListener('load', init);
-function generateStatistics(initialRevenue,totalSchools,supportRevenue,year){
+function generateStatistics(year,revenueCost,schoolCount){
     var outputParagraph = document.getElementById("statistics");
-    outputParagraph.innerHTML = "Year "+year+":<br>License Revenue: $"+initialRevenue.toLocaleString('en')+"<br>Total Schools: "+totalSchools+"<br>Support Services Revenue: $"+parseInt(supportRevenue).toLocaleString('en');
+    var totalSchools = schoolCount;
+    outputParagraph.innerHTML = "Year "+year+"<br>Total Schools: "+totalSchools+"<br>Consulting Revenue: $"+revenueCost[0].toLocaleString('en')+"<br>Licensing Revenue: $"+parseInt(revenueCost[1]).toLocaleString('en')+"<br>Consulting Fees: $"+parseInt(revenueCost[2]).toLocaleString('en')+"<br> Licensing Fees: $"+parseInt(revenueCost[3]).toLocaleString('en');
 }
 function init(){
 //document.getElementById("output").innerHTML = example(0);
